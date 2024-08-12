9f766134489c0855f964284ed4c330c822cd4c97
muhammad-salem-coder <muh.salem1403@gmail.com>
2024-08-12 23:05:20 +0400

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/curtain_website/',
+  build: {
+    outDir: 'dist' // or 'build' if you changed it as discussed earlier
+  },
   plugins: [react()],
 })
