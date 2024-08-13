aa1629bde222c8fddf9002114fb20643aefed173
muhammad-salem-coder <muh.salem1403@gmail.com>
2024-08-13 15:20:06 +0400

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/curtain_website/',
+  base: '/', // Change this if you're serving from the root
   build: {
-    outDir: 'dist' // or 'build' if you changed it as discussed earlier
+    outDir: 'dist'
   },
   plugins: [react()],
 })
